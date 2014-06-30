9b31593612595efc1bcce8fa9b768baac490542d
SeanZ <zhangxu0612@gmail.com>
2014-06-30 21:30:33 +0800

    add LRU Cache

---
+++ solutions/LRUCache.h
@@ -0,0 +1,14 @@
+//
+//  LRUCache.h
+//  MyLeetCodeOjSolutions
+//
+//  Created by zhangxu on 30/6/14.
+//  Copyright (c) 2014 zhangxu. All rights reserved.
+//
+
+#ifndef MyLeetCodeOjSolutions_LRUCache_h
+#define MyLeetCodeOjSolutions_LRUCache_h
+
+
+
+#endif
